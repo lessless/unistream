21c8b189ccfaf8316fa8f80815fc7ecc2bbcfe5d
lessless <yevhenii.kurtov@gmail.com>
2020-05-24 15:16:09 +0300

    Add invoice cancelation stage

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Unistream
 
-This code helps to reproduce `{:error, %GRPC.RPCError{message: ":stream_error: :closed", status: 13}}` error while subscribing to unidrectional server -> client stream 
+This code helps to reproduce `{:error, %GRPC.RPCError{message: ":stream_error: :closed", status: 13}}` error while sending message with `0` values
+
 
 ## Steps to reproduce
 
@@ -11,6 +12,6 @@ This code helps to reproduce `{:error, %GRPC.RPCError{message: ":stream_error: :
 5. Fetch dependencies `mix deps.get`
 5. Edit TLS  and Macaroon file locations if necessary in [lib/unistream.ex](https://github.com/lessless/unistream/blob/master/lib/unistream.ex)
 6. Start app `iex -S mix`
-7. Run `Unistream.subscribe` while in Elixir shell
-8. Observe error
+7. Run `Unistream.working_subscription` and observe `{:ok, <Stream>}` tuple
+8. Run `Unistream.crashing_subscription` and observe error
 
--- lib/unistream.ex
+++ lib/unistream.ex
@@ -1,16 +1,29 @@
 defmodule Unistream do
-  def subscribe do
-    {:ok, chan} = GRPC.Stub.connect("localhost:10001", cred: creds()) |> IO.inspect(label: "connect: ")
-    
-    Lnrpc.Lightning.Stub.subscribe_invoices(chan, Lnrpc.InvoiceSubscription.new(), metadata: %{macaroon: macaroon()})
+  def working_subscription() do
+    chan = setup()
+    Lnrpc.Lightning.Stub.subscribe_invoices(chan, Lnrpc.InvoiceSubscription.new(add_index: 1, settle_index: 1), metadata: %{macaroon: macaroon()}) 
+  end
+
+  def crashing_subscription() do
+    chan = setup()
+    Lnrpc.Lightning.Stub.subscribe_invoices(chan, Lnrpc.InvoiceSubscription.new(add_index: 0, settle_index: 0), metadata: %{macaroon: macaroon()}) 
   end
   
-  def creds() do
+  defp setup() do
+    {:ok, chan} = GRPC.Stub.connect("localhost:10001", cred: creds())
+    Lnrpc.Lightning.Stub.add_invoice(chan, Lnrpc.Invoice.new(%{value: 1000, memo: "THIS INVOICE WILL BE IN OPEN STATE"}), metadata: %{macaroon: macaroon()})
+    {:ok, invoice} = Lnrpc.Lightning.Stub.add_invoice(chan, Lnrpc.Invoice.new(%{value: 1000, memo: "THIS INVOICE WILL NOT BE SHOWN"}), metadata: %{macaroon: macaroon()})
+    Invoicesrpc.Invoices.Stub.cancel_invoice(chan, Invoicesrpc.CancelInvoiceMsg.new(payment_hash: invoice.r_hash), metadata: %{macaroon: macaroon()})
+
+    chan
+  end
+  
+  defp creds() do
     GRPC.Credential.new(ssl: [cacertfile: System.user_home() <> "/.polar/networks/1/volumes/lnd/alice/tls.cert"]) 
 
   end
   
-  def macaroon() do
+  defp macaroon() do
     File.read!(System.user_home() <> "/.polar/networks/1/volumes/lnd/alice/data/chain/bitcoin/regtest/admin.macaroon") |> Base.encode16()
 
   end
